d46e6086824442b03a7224a4812336bc9b404be4
lcm <1137909852@qq.com>
2023-03-14 16:26:38 +0800

    roll back cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <regex>
 
 #include "global.h"
-// #include "hdf5Utils.hpp"
+#include "hdf5Utils.hpp"
 #include "iso_common.h"
 #include "iso_method_ours.h"
 #include "json.hpp"
@@ -171,7 +171,7 @@ void run(std::string &dataDirPath)
             loadParticlesFromCSV(dataPath, particles, radiuses);
             break;
         case 1:
-            // readShonDyParticleData(dataPath, particles, radiuses, SCALE);
+            readShonDyParticleData(dataPath, particles, radiuses, SCALE);
             break;
         default:
             printf("ERROR: Unknown DATA TYPE;");
